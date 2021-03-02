ae208198b6d1830336006f46f1d106fbc34655df
jianrentang <freetyc@foxmail.com>
2021-03-02 16:02:47 +0800

    MOD: init account and kv when app start

--- app/src/main/java/com/w2solo/android/app/DreamlandApp.kt
+++ app/src/main/java/com/w2solo/android/app/DreamlandApp.kt
@@ -2,15 +2,23 @@ package com.w2solo.android.app
 
 import android.app.Application
 import com.w2solo.android.BuildConfig
+import com.w2solo.android.app.account.AccountManager
+import com.w2solo.android.module.keyvalue.KV
 import com.w2solo.android.utils.Logger
 
 class DreamlandApp : Application() {
     override fun onCreate() {
         super.onCreate()
-
         AppHolder.init(this)
         Logger.setEnabled(BuildConfig.DEBUG)
+        initSDKs()
     }
 
-    private fun initSDKs() {}
+    private fun initSDKs() {
+        //init kv
+        KV.init(this) { "${AccountManager.getInstance().loginUserID}" }
+        //init account
+        AccountManager.getInstance()
+
+    }
 }
