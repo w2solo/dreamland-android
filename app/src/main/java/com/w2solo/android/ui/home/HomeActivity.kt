aa586fb52bdd8c69aaf5ed8db59f2d3b46b8eb60
jianrentang <freetyc@foxmail.com>
2021-03-02 20:27:28 +0800

    MOD: 完善主界面

--- app/src/main/java/com/w2solo/android/ui/home/HomeActivity.kt
+++ app/src/main/java/com/w2solo/android/ui/home/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.w2solo.android.ui.home
 
 import android.os.Bundle
+import android.view.View
 import androidx.viewpager2.widget.ViewPager2
 import com.w2solo.android.R
 import com.w2solo.android.ui.base.activity.BaseToolbarActivity
@@ -14,5 +15,31 @@ class HomeActivity : BaseToolbarActivity() {
         super.onCreate(savedInstanceState)
         viewPager2 = fview(R.id.viewpager)
         viewPager2.adapter = HomePagerAdapter(this)
+
+        fview<View>(R.id.tab_topics).setOnClickListener {
+            setTab(0)
+        }
+        fview<View>(R.id.tab_discover).setOnClickListener {
+            setTab(1)
+        }
+        fview<View>(R.id.tab_me).setOnClickListener {
+            setTab(2)
+        }
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setTab(position)
+            }
+        })
+    }
+
+    private fun setTab(index: Int) {
+        fview<View>(R.id.tab_topics).isSelected = index == 0
+        fview<View>(R.id.tab_discover).isSelected = index == 1
+        fview<View>(R.id.tab_me).isSelected = index == 2
+        if (viewPager2.currentItem == index) {
+            return
+        }
+        viewPager2.currentItem = index
     }
 }
