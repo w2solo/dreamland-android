acf5cc5db9049fa47832fe0d60b7f885d48ecb6e
jianrentang <freetyc@foxmail.com>
2021-03-05 00:39:02 +0800

    MOD: 帖子跳转兼容

--- app/src/main/java/com/w2solo/android/ui/topic/markdown/AppLinkResolver.kt
+++ app/src/main/java/com/w2solo/android/ui/topic/markdown/AppLinkResolver.kt
@@ -1,5 +1,6 @@
 package com.w2solo.android.ui.topic.markdown
 
+import android.content.Context
 import android.view.View
 import com.w2solo.android.ui.topic.detail.TopicDetailFragment
 import com.w2solo.android.utils.AppLog
@@ -11,24 +12,38 @@ class AppLinkResolver : LinkResolver {
     private val defaultResolver = LinkResolverDef()
 
     override fun resolve(view: View, link: String) {
-        AppLog.d("MarkWon", "AppLinkResolver==resolve $link $LINK_PREFIX_TOPIC")
-        if (link.startsWith(LINK_PREFIX_TOPIC)) {//topic
-            val start = LINK_PREFIX_TOPIC.length
+        AppLog.d("MarkWon", "AppLinkResolver==resolve $link")
+        if (filterTopic(view.context, link, LINK_PREFIX_TOPIC)
+            || filterTopic(view.context, link, LINK_PREFIX_TOPIC2)
+            || filterTopic(view.context, link, LINK_PREFIX_TOPIC3)
+        ) {
+            return
+        }
+        defaultResolver.resolve(view, link)
+    }
+
+    private fun filterTopic(context: Context, link: String, prefix: String): Boolean {
+        if (link.startsWith(prefix)) {
             try {
+                val start = prefix.length
                 val idStr = link.substring(start, link.length)
                 val topicId = idStr.toLong()
-                TopicDetailFragment.showTopic(view.context, topicId)
+                TopicDetailFragment.showTopic(context, topicId)
+                return true
             } catch (thr: Throwable) {
                 thr.printStackTrace()
-                defaultResolver.resolve(view, link)
             }
-        } else {
-            defaultResolver.resolve(view, link)
         }
+        return false
     }
 
     companion object {
         private const val W2SOLO_HOST = "https://www.w2solo.com/"
+        private const val W2SOLO_HOST2 = "https://w2solo.com/"
+        private const val W2SOLO_HOST3 = "https://indiehackers.net/"
+
         const val LINK_PREFIX_TOPIC = "${W2SOLO_HOST}topics/"
+        const val LINK_PREFIX_TOPIC2 = "${W2SOLO_HOST2}topics/"
+        const val LINK_PREFIX_TOPIC3 = "${W2SOLO_HOST3}topics/"
     }
 }
